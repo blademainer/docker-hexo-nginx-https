11159c021c687a835f783fe8710ca814b8b71d51
blademainer <blademainer@gmail.com>
2019-03-06 21:43:39 +0800

    acme use nginx

--- nginx/cron-acme.sh
+++ nginx/cron-acme.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# See: https://github.com/Neilpang/acme.sh/wiki/%E8%AF%B4%E6%98%8E#3-copy%E5%AE%89%E8%A3%85-%E8%AF%81%E4%B9%A6
 echo "DOMAIN: $DOMAIN"
 echo "DNS_TYPE: $DNS_TYPE"
 
