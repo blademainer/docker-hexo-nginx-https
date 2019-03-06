d5583093b89b5bd4219f7ce1e0467467f1ed2e5f
blademainer <blademainer@gmail.com>
2019-03-06 21:36:39 +0800

    acme use nginx

--- nginx/cron-acme.sh
+++ nginx/cron-acme.sh
@@ -11,9 +11,10 @@ CERT_FILE="/nginx/ssl/${DOMAIN}.cer"
 FULLCHAIN_FILE="/nginx/ssl/${DOMAIN}-fullchain.cer"
 KEY_FILE="/nginx/ssl/${DOMAIN}.key"
 
-~/.acme.sh/acme.sh --issue --dns ${DNS_TYPE} -d $DOMAIN $RUNNING_ARGS $@
+#~/.acme.sh/acme.sh --issue --dns ${DNS_TYPE} -d $DOMAIN $RUNNING_ARGS $@
+~/.acme.sh/acme.sh --issue --webroot /nginx/html -d $DOMAIN $RUNNING_ARGS $@
 
 cp ~/.acme.sh/$DOMAIN/fullchain.cer $FULLCHAIN_FILE
 cp ~/.acme.sh/$DOMAIN/$DOMAIN.key $KEY_FILE
 echo "cer files: `ls /nginx/ssl/`"
-nginx -s reload
+nginx -s force-reload
